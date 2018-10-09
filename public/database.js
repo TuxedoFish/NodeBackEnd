85bb1efee4fd56787331792532a58ec837ebbf0b
TuxedoFish <harryliversedge@gmail.com>
2018-10-09 18:16:23 +0100

    added gitignore

--- public/database.js
+++ public/database.js
@@ -1,2 +1,12 @@
 //database.js
 //logic to run the back end matching algorithm
+
+//requires firebase module
+var admin = require('firebase-admin');
+//loads in the account key password
+var serviceAccount = require('path/to/serviceAccountKey.json');
+//initialises a firebase app with the credentials
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: 'https://<DATABASE_NAME>.firebaseio.com'
+});
