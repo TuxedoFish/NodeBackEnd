3098a09a5b9c1783547a88823913ac7e3620c09e
TuxedoFish <harryliversedge@gmail.com>
2018-10-10 21:56:23 +0100

    temporary test

--- server.js
+++ server.js
@@ -1,3 +1,28 @@
+//database.js
+//logic to run the back end matching algorithm
+
+//requires firebase module
+var admin = require('firebase-admin');
+//initialises a firebase app with the credential
+admin.initializeApp({
+  credential: admin.credential.cert({
+    "private_key": process.env.FIREBASE_CLIENT_SECRET,
+    "client_email": process.env.FIREBASE_CLIENT_EMAIL,
+  }),
+  databaseURL: "https://socialapp-575bc.firebaseio.com"
+});
+//get access to firestore from initialised app
+var db = admin.firestore();
+//TEST
+  var docRef = db.collection('USERS').doc('SOME_ARBITARY_DATA');
+
+  var setAda = docRef.set({
+    first: 'Ada',
+    last: 'Lovelace',
+    born: 1815
+  });
+//TEST
+
 // server.js
 // where your node app starts
 
@@ -8,7 +33,6 @@ var randomstring = require("randomstring");
 const util = require('util');
 var moment = require('moment');
 const nodeRequest = require('request');
-var db = require('/public/db/database.js');
 
 const client_id = process.env.OAUTH_CLIENT_ID;
 const client_secret = process.env.OAUTH_CLIENT_SECRET;
