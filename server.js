8a84cf191f2d03f38d0f427bcf9da51205730fcd
TuxedoFish <harryliversedge@gmail.com>
2018-10-11 10:08:50 +0100

    LEFT OUT A SEMICOLON

--- server.js
+++ server.js
@@ -3,7 +3,7 @@
 
 //requires firebase module
 var admin = require('firebase-admin');
-var key = process.env.FIREBASE_PRIVATE_KEY
+var key = process.env.FIREBASE_PRIVATE_KEY;
 key = key.replace("/\\n/g", "\n");
 //initialises a firebase app with the credential
 admin.initializeApp({
