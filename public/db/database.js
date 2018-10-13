45c8630bca8340faded48146527a916de07e8429
TuxedoFish <harryliversedge@gmail.com>
2018-10-13 11:21:10 +0100

    changed files around

--- public/db/database.js
+++ public/db/database.js
@@ -3,22 +3,30 @@
 
 //requires firebase module
 var admin = require('firebase-admin');
-//initialises a firebase app with the credential
-admin.initializeApp({
-  credential: admin.credential.cert({
-    "private_key": process.env.FIREBASE_CLIENT_SECRET,
-    "client_email": process.env.FIREBASE_CLIENT_EMAIL,
-  }),
-  databaseURL: "https://socialapp-575bc.firebaseio.com"
-});
+var key = JSON.parse(process.env.FIREBASE_PRIVATE_KEY)["key"];
+
+module.exports() = {
+	initFirebase: function() {
+		//initialises a firebase app with the credential
+		admin.initializeApp({
+		  credential: admin.credential.cert({
+		    "private_key": key,
+		    "client_email": process.env.FIREBASE_CLIENT_EMAIL,
+		    "project_id": process.env.FIREBASE_PROJECT_ID,
+		    "private_key_id": process.env.FIREBASE_PRIVATE_KEY_ID
+		  }),
+		  databaseURL: "https://socialapp-575bc.firebaseio.com"
+		});
+	}
+	};
 //get access to firestore from initialised app
 var db = admin.firestore();
 //TEST
-	var docRef = db.collection('USERS').doc('SOME_ARBITARY_DATA');
+  var docRef = db.collection('USERS').doc('SOME_ARBITARY_DATA');
 
-	var setAda = docRef.set({
-	  first: 'Ada',
-	  last: 'Lovelace',
-	  born: 1815
-	});
+  var setAda = docRef.set({
+    first: 'Ada',
+    last: 'Lovelace',
+    born: 1815
+  });
 //TEST
--- server.js
+++ server.js
@@ -8,33 +8,9 @@ var randomstring = require("randomstring");
 const util = require('util');
 var moment = require('moment');
 const nodeRequest = require('request');
+var db = require('/public/db/database.js');
 
-//database.js
-//logic to run the back end matching algorithm
-
-//requires firebase module
-var admin = require('firebase-admin');
-//initialises a firebase app with the credential
-admin.initializeApp({
-  credential: admin.credential.cert({
-    "private_key": JSON.parse((process.env.FIREBASE_PRIVATE_KEY).replace('/ 0+(?![/. }])/g', ' ')),
-    "client_email": process.env.FIREBASE_CLIENT_EMAIL,
-    "project_id": process.env.FIREBASE_PROJECT_ID,
-    "private_key_id": process.env.FIREBASE_PRIVATE_KEY_ID
-  }),
-  databaseURL: "https://socialapp-575bc.firebaseio.com"
-});
-//get access to firestore from initialised app
-var db = admin.firestore();
-//TEST
-  var docRef = db.collection('USERS').doc('SOME_ARBITARY_DATA');
-
-  var setAda = docRef.set({
-    first: 'Ada',
-    last: 'Lovelace',
-    born: 1815
-  });
-//TEST
+db.initFirebase();
 
 // server.js
 const client_id = process.env.OAUTH_CLIENT_ID;
