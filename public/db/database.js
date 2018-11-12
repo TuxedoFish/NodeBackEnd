c098e12900b22791e1d381083cdc54ac807254f9
TuxedoFish <harryliversedge@gmail.com>
2018-11-12 11:05:13 +0000

    stupid error constant NOT string literal

--- public/db/database.js
+++ public/db/database.js
@@ -59,7 +59,7 @@ function listenForRequests() {
 								//Tests if there is a strong match between the group and the searching user
 								if(isMatchStrong()) {
 									//Logic here to add a user to an existing group
-									addIntoGroup(docs[i], spaces[j][id]);
+									addIntoGroup(docs[i], spaces[j][ID]);
 									resolved = true;
 									spaces[j][SPACE_LEFT] --;
 								} else {
